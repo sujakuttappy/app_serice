084d56f856b298ded77b182b5170a0fc404118cc
sujakuttappy <sujakuttappy@gmail.com>
2019-09-24 17:39:19 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "client_id" {}
-variable "client_secret" {}
-variable "subscription_id" {}
-variable "tenant_id" {}
-variable "resource_group" {}
-variable "location" {}
-variable "count" {}
-variable "linux_version" {}
+variable "client_id" {default = "d0715cbd-a894-40da-bc2b-6c7f7d69da71"}
+variable "client_secret" {default = "IRAf5tcK@-2yjEWN@sas0-0Tva/jSG*?"}
+variable "subscription_id" {default = "878d3f24-1f11-4f9d-ba0b-55350ee0e2ba"}
+variable "tenant_id" {default = "2536b97b-4aeb-4424-9b0e-882569d44587"}
+variable "resource_group" {default = "devops"}
+variable "location" {default = "eastus"}
+variable "count" {default ="1"}
+variable "linux_version" {default = "PYTHON|3.7"}
